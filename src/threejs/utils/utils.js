278588ec69b1508e7b50b0647f318288a965b8e8
Ranen <ranensim.yangyi@gmail.com>
2025-01-19 00:01:33 +0800

    refactor: reorganise files for seperate stylesheets & fix favicon file paths

--- src/threejs/utils/utils.js
+++ src/threejs/utils/utils.js
@@ -1,3 +1,3 @@
-export const BASE_PATH = import.meta.env.BASE_URL?.startsWith('/')
+export const BASE_PATH = typeof import.meta.env !== 'undefined' && import.meta.env.BASE_URL?.startsWith('/')
   ? import.meta.env.BASE_URL
   : "/";
