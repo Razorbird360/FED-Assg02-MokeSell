eeb46cb6743ac2523115d7d4219dd57b26a961b6
Ranen <ranensim.yangyi@gmail.com>
2025-01-26 05:03:20 +0800

    feat: added menu popup for mobile view

---
+++ src/main.js
@@ -0,0 +1,14 @@
+document.querySelector('.mobile_menu_trigger').addEventListener('click', function() {
+    const mobileMenu = document.querySelector('.mobile_menu');
+    
+    if (mobileMenu.classList.contains('active')) {
+        mobileMenu.classList.add('closing');
+        
+        // Wait for animations to finish before removing active class
+        setTimeout(() => {
+            mobileMenu.classList.remove('active', 'closing');
+        }, 300);
+    } else {
+        mobileMenu.classList.add('active');
+    }
+});
